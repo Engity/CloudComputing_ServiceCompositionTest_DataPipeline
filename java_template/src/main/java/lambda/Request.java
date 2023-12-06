f6913ba04b2a07b99f2ad2ab3dae2a145aed2535
TN <ngToan1107@gmail.com>
2023-12-05 16:07:53 -0800

    Setup dependencies

--- java_template/src/main/java/lambda/Request.java
+++ java_template/src/main/java/lambda/Request.java
@@ -7,17 +7,53 @@ package lambda;
 public class Request {
 
     String name;
+    private String bucketname;
+    private String filename;
+    private int row;
+    private int col;
 
     public String getName() {
         return name;
     }
-    
+
     public String getNameALLCAPS() {
         return name.toUpperCase();
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getBucketname() {
+        return bucketname;
+    }
+
+    public void setBucketname(String name) {
+        this.bucketname = name;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String name) {
+        this.filename = name;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int c) {
+        this.col = c;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int r) {
+        this.row = r;
     }
 
     public Request(String name) {
