75f9a9fb62a546bef37a1449ef8ffbfdafc824b9
aaront99 <aaron102499@gmail.com>
2023-12-07 16:20:54 -0800

    Implement Load, Added bash to run T-L

--- java_template/src/main/java/lambda/Request.java
+++ java_template/src/main/java/lambda/Request.java
@@ -7,17 +7,62 @@ package lambda;
 public class Request {
 
     String name;
+    private String bucketname;
+    private String filename;
+    private int operationType;
+    private int row;
+    private int col;
 
     public String getName() {
         return name;
     }
-    
+
     public String getNameALLCAPS() {
         return name.toUpperCase();
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getBucketname() {
+        return bucketname;
+    }
+
+    public void setBucketname(String name) {
+        this.bucketname = name;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String name) {
+        this.filename = name;
+    }
+
+     public int getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(int c) {
+        this.operationType = c;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int c) {
+        this.col = c;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int r) {
+        this.row = r;
     }
 
     public Request(String name) {
--- java_template/test/callservice.sh
+++ java_template/test/callservice.sh
@@ -1,20 +1,82 @@
 #!/bin/bash
 
+#!/bin/bash
+bucketName="tcss462salesdata"
+functionName="switchBoard"
+
+echo "Which file would you like to use?"
+echo "1. 100"
+echo "2. 1000"
+echo "3. 10000"
+echo "4. 50000"
+echo "5. 100000"
+echo "6. 500000"
+echo "7. 1000000"
+echo "8. 1500000"
+
+read -p "Enter your option " option
+
+case $option in
+    1) 
+    option=100
+    ;;
+
+    2) 
+    option=1000
+    ;;
+
+    3) 
+    option=10000
+    ;;
+
+    4) 
+    option=50000
+    ;;
+
+    5) 
+    option=100000
+    ;;
+
+    6) 
+    option=500000
+    ;;
+
+    7) 
+    option=1000000
+    ;;
+
+    8) 
+    option=1500000
+    ;;
+
+    *)
+    echo "Entered option is invalid, we will use the default csv file with 100 records"
+    option=100
+    ;;
+
+esac
+
+
 # JSON object to pass to Lambda Function
-json={"\"name\"":"\"Susan\u0020Smith\",\"param1\"":1,\"param2\"":2,\"param3\"":3}
+fileName=${option}"SalesRecords.csv"
 
-#echo "Invoking Lambda function using API Gateway"
-#time output=`curl -s -H "Content-Type: application/json" -X POST -d $json {API-GATEWAY-REST-URL}`
-#echo ""
+echo You choose the file ${filename} 
+jsonT={"\"row\"":50,"\"col\"":10,"\"bucketname\"":\"${bucketName}\"","\"filename\"":\"${fileName}\""}
+echo $jsonT
 
-#echo ""
-#echo "JSON RESULT:"
-#echo $output | jq
-#echo ""
 
+echo "Invoking Process CSV Lambda function using AWS CLI"
+time output=`aws lambda invoke --invocation-type RequestResponse --function-name switchBoard --region us-east-2 --payload $jsonT --cli-connect-timeout 900 --cli-read-timeout 900 /dev/stdout | head -n 1 | head -c -2 ; echo`
+echo ""
+echo "JSON RESULT:"
+echo $output | jq
+
+#process Function procecssCSVTut5
+
+jsonL={"\"bucketname\"":\"${bucketName}\"}
 echo "Invoking Lambda function using AWS CLI"
 #time output=`aws lambda invoke --invocation-type RequestResponse --function-name {LAMBDA-FUNCTION-NAME} --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
-time output=`aws lambda invoke --invocation-type RequestResponse --function-name hellomysqlf21 --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+time output=`aws lambda invoke --invocation-type RequestResponse --function-name processSalesData --region us-east-2 --payload $jsonL /dev/stdout | head -n 1 | head -c -2 ; echo`
 
 echo ""
 echo "JSON RESULT:"
